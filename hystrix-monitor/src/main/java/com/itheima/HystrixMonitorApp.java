3ff8aee66a2d2363fb336e722579c755decf0bb4
yanzu <1245673955@qq.com>
2021-12-04 09:25:09 +0800

    图形监控monitor搭建

---
+++ hystrix-monitor/src/main/java/com/itheima/HystrixMonitorApp.java
@@ -0,0 +1,34 @@
+package com.itheima;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+
+@EnableTurbine //开启Turbine 很聚合监控功能
+@EnableHystrixDashboard //开启Hystrix仪表盘监控功能
+public class HystrixMonitorApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixMonitorApp.class, args);
+    }
+
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
+
